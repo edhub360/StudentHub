4d75a0156d3d2d7ffaae09de1b1171681a31e7f1
jananiedhub360 <janani.edhub360@gmail.com>
2025-10-30 16:03:53 +0530

    Update Register.tsx

--- src/Components/Register.tsx
+++ src/Components/Register.tsx
@@ -6,7 +6,7 @@ interface RegisterProps {
   onRegisterSuccess?: (token: string, userId: string, hasSubscription: boolean) => void;
 }
 
-const API_BASE_URL = 'http://127.0.0.1:8000';
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const Register: React.FC<RegisterProps> = ({ onSwitchToLogin, onRegisterSuccess }) => {
   const [name, setName] = useState('');
