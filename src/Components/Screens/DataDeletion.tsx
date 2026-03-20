489adfec8f03a349cb9841da3eb025a12c053ece
jananiedhub360 <jananiparameswaranp@gmail.com>
2026-03-20 15:23:43 +0530

    privacy policy, term of service, data deletion reworked for facebook

--- src/Components/Screens/DataDeletion.tsx
+++ src/Components/Screens/DataDeletion.tsx
@@ -1,21 +1,8 @@
-// src/pages/DataDeletion.tsx
+import { useEffect } from 'react';
+
 export default function DataDeletion() {
-  return (
-    <div style={{ maxWidth: 600, margin: "60px auto", padding: "0 20px" }}>
-      <h1>Data Deletion Instructions</h1>
-      <p>
-        If you have used Facebook Login on Edhub360 and would like to request
-        deletion of your data, please follow these steps:
-      </p>
-      <ol>
-        <li>Send an email to <strong>jananiparameswaranece@gmail.com</strong></li>
-        <li>Use the subject line: <strong>"Facebook Data Deletion Request"</strong></li>
-        <li>Include your registered email address in the email</li>
-      </ol>
-      <p>
-        We will process your request and delete all associated data within
-        <strong> 30 days</strong>.
-      </p>
-    </div>
-  );
+  useEffect(() => {
+    window.location.href = '/data-deletion.html';
+  }, []);
+  return null;
 }
